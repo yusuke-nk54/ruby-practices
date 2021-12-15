fc792ebc5366efb81dbd8edd8d5c8c9e93f4c7cd
yusuke-nk54 <yusuke_nk54@yahoo.co.jp>
2021-12-15 22:12:44 +0900

    表示間隔

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,13 +1,22 @@
 
 ls = Dir.glob("*") #ファイルとディレクトリの表示
 p ls
+max_word = ls.max_by { |s| s.length }
+p max_word
+p max_word.size
+
+space = " "
+# ls.each do |x|
+#   puts space * (max_word.size - x.length)
+# end
 
 ls.each_with_index do |name ,idx|
   # puts "name=#{name}"
   # puts "idx=#{idx}"
   if idx % 3 == 2
-  print name + "\n"
+    print name + "\n"
   else
-  print name + "             "
+    print name + (space * (max_word.size - name.length)) + "   "
   end
 end
+puts "\n"
