c8c986c72b1b8a3543d3089251e13ce2bd4ba1d7
yusuke-nk54 <yusuke_nk54@yahoo.co.jp>
2021-12-16 22:46:23 +0900

    lsコマンド1

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,47 +1,27 @@
+# frozen_string_literal: true
 
-ls = Dir.glob("*") #ファイルとディレクトリの表示
-p ls
-max_word = ls.max_by { |s| s.length }
-p max_word
-p max_word.size
+ls = Dir.glob('*')
 
-space = " "
-# ls.each do |x|
-#   puts space * (max_word.size - x.length)
-# end
+max_word = ls.max_by(&:length)
 
-ls.each_with_index do |name ,idx|
-  # puts "name=#{name}"
-  # puts "idx=#{idx}"
-  if idx % 3 == 2
-    print name + "\n"
-  else
-    print name + (space * (max_word.size - name.length)) + "   "
-  end
-end
-puts "\n"
-
-
-
-mold = []
-ls.each_slice(3) do |l|
-  mold << l
+ls_array = []
+ls.each_slice(3) do |a|
+  ls_array << a
 end
 
-flatten_mold = mold.map {|m| m.values_at(0...3)}.flatten
+flatten_ls_array = ls_array.map { |m| m.values_at(0...3) }.flatten
 
-# p mold.flatten
-
-car = []
-flatten_mold.each_slice(flatten_mold.size / 3) do |o|
-  car << o
+ls_array_slice = []
+flatten_ls_array.each_slice(flatten_ls_array.size / 3) do |b|
+  ls_array_slice << b
 end
-neko = car.transpose
-p neko
+ls_list = ls_array_slice.transpose
 
-neko.each do |m|
-  m.each do |k|
-    print k.to_s.ljust(24)
+space = ' '
+
+ls_list.each do |list|
+  list.each do |n|
+    print "#{n}#{space * (max_word.size - n.to_s.length)}   "
   end
   print "\n"
 end
